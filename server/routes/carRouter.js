4303474a8c9bb13803e9b9863894ee2e3c45117f
Pranish Bajracharya <pranish2017bajracharya@gmail.com>
2024-03-14 16:09:54 +0545

    changes in routes and utils

--- server/routes/carRouter.js
+++ server/routes/carRouter.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { createNewCarDetails, getAllCars } = require('../controller/carController');
+const upload = require('../utils/fileUpload');
 
-router.post('/create-car', createNewCarDetails);
+router.post('/create-car', upload.single('image'), createNewCarDetails);
 router.get('/get-all-cars', getAllCars);
 
 module.exports = router;
