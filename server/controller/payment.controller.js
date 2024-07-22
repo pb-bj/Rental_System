987ee5e2b87538d51bc8a59505cdf540b065d897
pb-bj <pranish2017bajracharya@gmail.com>
2024-07-22 18:09:57 +0545

    payment success message updated and added spinner

--- server/controller/payment.controller.js
+++ server/controller/payment.controller.js
@@ -50,17 +50,19 @@ export const createPayment = async (req, res) => {
 
 export const getUserPaymentDetail = async (req, res) => {
     try {
-        const { userId } = req.params;
+        const userId = req.params.id;
          if (!mongoose.Types.ObjectId.isValid(userId)) {
             return res.status(400).json({ message: 'Invalid User ID' });
         }
         
-        const user = await User.findById(userId);
+        const user = await User.findById(userId)
         if (!user) return res.status(404).json({ message: 'User not found' });
 
-        const payment = await Payment.find({ user: userId });
-        if (!payment) return res.status(404).json({ message: 'No payment found' });
-
+        const payment = await Payment.find({ user: userId })
+            .populate('user', 'email');
+            if (!payment) return res.status(404).json({ message: 'No payment found' });
+            
+        console.log(payment);
         res.status(200).json({ message: 'User payment: ', payment });
     } catch (err) {
         console.log("Error");
